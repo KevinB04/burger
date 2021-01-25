721e406bd3e1d101ed3a6127b8c6c9689c504cf2
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-25 17:02:05 -0500

    fixing .sql files and creating code inside connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,9 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
 
+SELECT * FROM burgers;
