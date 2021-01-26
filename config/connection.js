7d6cf3d00af8af818e81f4cc8dc55d19c90254f8
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-26 09:25:47 -0500

    successfully added Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 // MySQL connection.
 var mysql = require("mysql");
-var connection; 
+let connection; 
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "RememberThis!4",
     database: "burgers_db"
   });
-};
+}
 
 // Connection.
 connection.connect(function(err) {
